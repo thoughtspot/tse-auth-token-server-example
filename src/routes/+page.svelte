151fbc94afe3fdfb7e04f81bc59baa4451e43d38
billdback-ts <bill.back@thoughtspot.com>
2023-09-27 11:08:32 -0400

    Migrating in code for the trusted auth example.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,18 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+    import './styles.css'
 </script>
+<h1>Trusted authentication</h1>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<p>
+You've reached the root of the ThoughtSpot trusted authentication example.
+</p>
+<p>
+    To authentication with a user, call
+<span class="mono">https://&lt;auth-host&gt;/token/&lt;username&gt;</span> where:
+</p>
+<ul>
+    <li>The <span class="mono">username</span> is the name of the user to get.</li>
+</ul>
+<p>
+Current this service will always return a token for the given user assuming they exist and the secret key is properly set in the server environment.
+</p>
