1be982caee3e6191bf2eae0cbf5f59d475ef441b
Bill Back <32280587+billdback-ts@users.noreply.github.com>
2024-10-03 10:03:22 -0400

    Updating to support eLearning users.

--- src/routes/token/+server.js
+++ src/routes/token/+server.js
@@ -1,5 +1,5 @@
 import {error} from '@sveltejs/kit';
-import {TS_URL, ORG_ID, SECRET_KEY, PASSCODE} from '$env/static/private';
+import {TS_URL, TSE_ORG_ID, E_LEARNING_ORG_ID, SECRET_KEY, PASSCODE} from '$env/static/private';
 
 
 /** @type {import('./$types').RequestHandler} */
@@ -8,28 +8,29 @@ export async function GET({url}) {
 
   const username = url.searchParams.get('username');
   if (!username) {
-    throw error(400, 'username parameter is required, e.g. ${urlFormat}');
+    throw error(400, `username parameter is required, e.g. ${urlFormat}`);
   }
 
   const passcode = url.searchParams.get('passcode');
   if (!passcode) {
-    throw error(400, 'a valid passcode is required, e.g. ${urlFormat}');
+    throw error(400, `a valid passcode is required, e.g. ${urlFormat}`);
   }
   if (passcode !== PASSCODE) {
     throw error(400, `passcode is incorrect, e.g. ${urlFormat}'`);
   }
 
-  const userregx = /user[0-9][0-9][0-9]/;
-  if (!username.match(userregx)) {
-    throw error(403, 'only users in for the form of userNNN are allowed');
+  if (!is_valid_user(username)) {
+    throw error(403, `User ${username} is not valid.`);
   }
+
+  let org_id = get_org_id(username)
 
   const data = {
     "username": username,
     "validity_time_in_sec": 300,
-    "org_id": ORG_ID,
+    "org_id": org_id,
     "auto_create": false,
-    "secret_key": SECRET_KEY
+    "secret_key": SECRET_KEY  // to work this has to be the instance level so it works across orgs.
   }
 
   const resp = await fetch(`${TS_URL}/api/rest/2.0/auth/token/full`, {
@@ -47,10 +48,28 @@ export async function GET({url}) {
   let token;
   if (tokenbody['error']) { // means there was an error
     token = JSON.stringify(tokenbody);
-  }
-  else {
+  } else {
     token = tokenbody.token;
   }
 
   return new Response(`${JSON.stringify(token)}`);
+}
+
+const is_valid_user = (username) => {
+  // Returns true if this is a valid username.
+
+  // userNNN is for the advanced training.  tspractice1,2,3 etc. are for TSU.
+  const userregx = /user[0-9][0-9][0-9]/;
+  return username.match(userregx) || username.startsWith('tspractice');
+}
+
+const get_org_id = (username) => {
+  // Gets the org ID for the given user.  Right now only TSE and eLearning are supported.
+
+  if (username.startsWith('user')) {
+    return TSE_ORG_ID;
+  } else if (username.startsWith('tspractice')) {
+    return E_LEARNING_ORG_ID;
+  }
+  return -1  // will cause an error.
 }
