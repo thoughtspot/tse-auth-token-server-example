6159817ce815a3e77a9c6276bf347622ad5a4bb1
Bill Back <32280587+billdback-ts@users.noreply.github.com>
2025-04-09 11:34:35 -0400

    Updating valid user names
    Updating user names for TSU user.

--- src/routes/token/+server.js
+++ src/routes/token/+server.js
@@ -84,7 +84,7 @@ const is_valid_user = (username) => {
 
   const userregx = /user[0-9][0-9][0-9]/;
   return username.match(userregx)  // Users for TSE training
-    || username.startsWith('tspractice') // Users for TSU
+    || username === 'training1test' // Users for TSU
     || username === 'tsedev';  // Shared user for TSE training
 }
 
@@ -93,7 +93,7 @@ const get_org_id = (username) => {
 
   if (username.startsWith('user')) {
     return TSE_ORG_ID;
-  } else if (username.startsWith('tspractice')) {
+  } else if (username === 'training1test') {
     return E_LEARNING_ORG_ID;
   }
   return -1  // will cause an error.
